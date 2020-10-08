925d92dab9e5c2853b0fb4202484f24d6258e57b
Jussi <jussi@nevavuori.fi>
2020-10-08 16:50:38 +0300

    Updated authentication extraction

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -3,13 +3,52 @@ declare type EjsTemplateName = "forgot_password";
 declare namespace Express {
   export interface Request {
     data: {
+      /**
+       * If the user was found, their account is validated and the request
+       * had valid refresh and access tokens, the user who made the request
+       * can be found in this variable.
+       *
+       * Null means attempt was made to parse the user but failed.
+       *
+       * Undefined means no attempt was made to parse the user.
+       */
       user?: import("@prisma/client").User | null | undefined;
-      refreshToken?:
-        | import("../../services/RefreshToken").RefreshToken
-        | undefined;
-      accessToken?:
-        | import("../../services/AccessToken").AccessToken
-        | undefined;
+
+      /**
+       * Was an access token found in the request?
+       */
+      accessTokenFound?: boolean;
+
+      /**
+       * Was a refresh token found in the request?
+       */
+      refreshTokenFound?: boolean;
+
+      /**
+       * Was a user found with a valid access token and refresh token but
+       * the user was invalid, due to being blocker or having an unverified
+       * email?
+       */
+      blockedUser?: import("@prisma/client").User;
+
+      /**
+       * If the user is not found, save reason here
+       */
+      noUserReason?:
+        | "disabled"
+        | "email-not-verified"
+        | "invalid-tokens"
+        | "user-not-found";
+
+      /**
+       * The refresh token that was found in the request if valid.
+       */
+      refreshToken?: import("../../services/RefreshToken").RefreshToken;
+
+      /**
+       * The access token that was found in the request if valid.
+       */
+      accessToken?: import("../../services/AccessToken").AccessToken;
     };
   }
 }
--- src/middleware/extractAuthentication.ts
+++ src/middleware/extractAuthentication.ts
@@ -13,30 +13,47 @@ export function extractAuthentication() {
      * Mark user as null initially to signal that this middleware
      * has taken place and no user was found (unless one is found)
      */
-
     request.data.user = null;
 
     /**
-     * Get both tokens from request
+     * Get access token from request and verify
      */
-
     const accessToken = await AccessToken.fromRequest(request);
 
-    const refreshToken = await RefreshToken.fromRequest(request);
+    if (accessToken) {
+      request.data.accessTokenFound = true;
 
-    if (!accessToken || !refreshToken) {
-      return next();
+      const verified = await accessToken.verify();
+
+      if (verified) {
+        request.data.accessToken = accessToken;
+      }
+    } else {
+      request.data.accessTokenFound = false;
     }
 
     /**
-     * Check both tokens are verified
+     * Get refresh token from request and verify
      */
+    const refreshToken = await RefreshToken.fromRequest(request);
 
-    const accessTokenVerified = await accessToken.verify();
+    if (refreshToken) {
+      request.data.refreshTokenFound = true;
 
-    const refreshTokenVerified = await refreshToken.verify();
+      const verified = await refreshToken.verify();
 
-    if (!accessTokenVerified || !refreshTokenVerified) {
+      if (verified) {
+        request.data.refreshToken = refreshToken;
+      }
+    } else {
+      request.data.refreshTokenFound = false;
+    }
+
+    /**
+     * Ensure both tokens were found and validated
+     */
+    if (!request.data.refreshToken || !request.data.accessToken) {
+      request.data.noUserReason = "invalid-tokens";
       return next();
     }
 
@@ -44,23 +61,40 @@ export function extractAuthentication() {
      * Get the user by the access token UID and ensure they exist and
      * are not disabled
      */
+    const user = await prisma.user.findOne({
+      where: { id: request.data.accessToken.uid },
+    });
 
-    const user = await prisma.user.findOne({ where: { id: accessToken.uid } });
+    /**
+     * Ensure user exists
+     */
+    if (!user) {
+      request.data.noUserReason = "user-not-found";
+      return next();
+    }
 
-    if (!user || user.disabled || !user.emailVerified) {
+    /**
+     * Ensure user not disabled
+     */
+    if (user.disabled) {
+      request.data.blockedUser = user;
+      request.data.noUserReason = "disabled";
+      return next();
+    }
+
+    /**
+     * Ensure email is verified
+     */
+    if (!user.emailVerified) {
+      request.data.blockedUser = user;
+      request.data.noUserReason = "email-not-verified";
       return next();
     }
 
     /**
      * Apply found user to request and continue
      */
-
     request.data.user = user;
-
-    request.data.accessToken = accessToken;
-
-    request.data.refreshToken = refreshToken;
-
     next();
   };
 }
